7cd6fecae4c46b1367a29f982bbd5d461be7d4cf
lucas leal vale <lucaslealvale01@gmail.com>
2019-09-04 12:03:34 -0300

    toplevel rubrica c

--- Projetos/C-LogicaCombinacional/src/TopLevel.vhd
+++ Projetos/C-LogicaCombinacional/src/TopLevel.vhd
@@ -37,12 +37,35 @@ architecture rtl of TopLevel is
 -- signals
 --------------
 
+
+
+
 ---------------
 -- implementacao
 ---------------
 begin
- 
-  LEDR(0) <= SW(1) or SW (0);
-  LEDR(1) <= SW(1);
-  HEX0(6 downto 0)<="0010010";
+ HEX0 <= '1000000' when (sw(3 downto 0) = "0000") else
+ 		'1111001' when (sw(3 downto 0) = "0001") else
+ 		'0100100' when (sw(3 downto 0) = "0010") else
+ 		'0110000' when (sw(3 downto 0) = "0011") else
+ 		'0011001' when (sw(3 downto 0) = "0100") else
+ 		'0010010' when (sw(3 downto 0) = "0101") else
+ 		'0000010' when (sw(3 downto 0) = "0110") else
+ 		'1111000' when (sw(3 downto 0) = "0111") else
+ 		'0000000' when (sw(3 downto 0) = "1000") else
+ 		'0010000' when (sw(3 downto 0) = "1001") else
+ 		'0001000' when (sw(3 downto 0) = "1010") else
+ 		'0000011' when (sw(3 downto 0) = "1011") else
+ 		'1000110' when (sw(3 downto 0) = "1100") else
+ 		'0100001' when (sw(3 downto 0) = "1101") else
+ 		'0000110' when (sw(3 downto 0) = "1110") else
+ 		'0001110';
+ 	
+
+
+
+
+ -- LEDR(0) <= SW(1) or SW (0);
+ -- LEDR(1) <= SW(1);
+ -- HEX0(6 downto 0)<="0010010";
 end rtl;
