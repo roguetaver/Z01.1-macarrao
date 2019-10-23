e48e08941c31acdc43b1615bfebe5f59bbb6271e
borg <you@example.com>
2019-10-23 15:22:13 -0300

    cpu

--- Projetos/G-Computador/src/rtl/CPU.vhd
+++ Projetos/G-Computador/src/rtl/CPU.vhd
@@ -106,7 +106,44 @@ architecture arch of CPU is
 
   signal s_pcout: STD_LOGIC_VECTOR(15 downto 0);
 
+  
+
+  port(
+    clock:       in  STD_LOGIC;                        -- sinal de clock para CPU
+    reset:       in  STD_LOGIC;                        -- reinicia toda a CPU (inclusive o Program Counter)
+    inM:         in  STD_LOGIC_VECTOR(15 downto 0);    -- dados lidos da memória RAM
+    instruction: in  STD_LOGIC_VECTOR(17 downto 0);    -- instrução (dados) vindos da memória ROM
+    outM:        out STD_LOGIC_VECTOR(15 downto 0);    -- dados para gravar na memória RAM
+    writeM:      out STD_LOGIC;                        -- faz a memória RAM gravar dados da entrada
+    addressM:    out STD_LOGIC_VECTOR(14 downto 0);    -- envia endereço para a memória RAM
+    pcout:       out STD_LOGIC_VECTOR(14 downto 0)     -- endereço para ser enviado a memória ROM
+    );
 
 begin
+
+ULA: ALU port map(s_muxSDout,s_muxAMD_ALUout, c_zx, c_nx, c_zy, c_ny, c_f, c_no, c_zr, c_ng, s_ALUout);
+
+muxSD : Mux16 port map(s_regSout,s_regDout, c_muxSD_ALU, s_muxSDout);
+
+muxAMD : Mux16 port map(s_regDout, s_muxAM_out, c_muxAMD_ALU, s_muxAMD_ALUout);
+
+muxAM : Mux16 port map(s_regAout, inM, c_muxAM, s_muxAM_out);
+
+muxALU : Mux16 port map(s_ALUout, instruction(15 downto 0), c_muxALUI_A, s_muxALUI_Aout);
+
+regS : Register16 port map(clock, s_ALUout, c_loadS, s_regSout);
+
+regD : Register16 port map(clock, s_ALUout, c_loadD, s_regDout);
+
+regA : Register16 port map(clock, s_muxALUI_Aout, c_loadA, s_regAout);
+
+PC : PC port map(clock, '1', c_loadPC, reset, s_regAout, pcout);
+
+c_unit : ControlUnit port map(instruction, c_zr, c_ng, c_muxALUI_A, c_muxAM, c_muxAMD_ALU, 
+  c_muxSD_ALU, c_zx, c_nx, c_zy, c_ny, c_f, c_no, c_loadA, c_loadD, c_loadS, writeM ,c_loadPC);
+
+
+addressM <= s_regAout;
+outM <= s_ALUout;
 
 end architecture;
