24fb709044e1f80c0f4f27beb944f878e51b147e
borg <you@example.com>
2019-10-30 16:34:19 -0300

    and16 arrumado

--- Projetos/C-LogicaCombinacional/src/rtl/And16.vhd
+++ Projetos/C-LogicaCombinacional/src/rtl/And16.vhd
@@ -3,15 +3,14 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity And16 is
 	port ( 
-			x:   in  STD_LOGIC_vector(15 downto 0);
-			y:   in  STD_LOGIC_vector(15 downto 0);
-			q:   out STD_LOGIC_vector(15 downto 0));
+			a:   in  STD_LOGIC_VECTOR(15 downto 0);
+			b:   in  STD_LOGIC_VECTOR(15 downto 0);
+			q:   out STD_LOGIC_VECTOR(15 downto 0));
 end entity;
 
 architecture arch of And16 is
 begin
 
-q <= x and y;
-
+	q <= a and b;
 
 end architecture;
